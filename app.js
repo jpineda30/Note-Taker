03c2d67c22777a27acab1af41277a4804aa4044b
pepe30 <jose.pinedaesquer@gmail.com>
2020-12-15 10:25:52 -0600

    added routes to app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 var express = require("express");
 var path = require("path");
 
+var fs = require("fs");
+const { json } = require("express");
+const { parse } = require("path");
+
+
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -20,6 +25,34 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+///api/notes
+app.post("/api/notes",function(req,res){
+        var base = require("./db.json");
+        
+        base.push(req.body);
+        console.log(base);
+        
+        fs.writeFileSync(__dirname + '/db.json', JSON.stringify(base) ,function (error, data) {
+            if (error) {
+              return error
+            }
+            console.log("Saved!")
+        });
+        
+    res.write("OK");     
+});
+
+//get
+app.get("/api/notes", function(req, res) {
+  var base = require("./db.json");
+  let parsed = JSON.stringify(base);
+  res.send(parsed);
+
+});
 
 
 // Starts the server to begin listening
